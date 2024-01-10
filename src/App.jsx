e8ed12be83c206403287b6408d617577ce1c6fe0
Stefan Lange <stefanlange@maeve.de>
2024-01-10 14:54:39 +0100

    base-structure Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Data from "./components/Data";
+import { Header } from "./components/Header";
 
 function App() {
 
   return (
     <>
+      <Header />
       <h1>Sightguide</h1>
       <Data />
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { Search } from "./Search";
+export default function Header(){
+
+    return (
+        <div>
+        <Search />
+        </div>
+    );
+}
