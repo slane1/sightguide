f2452901211311856ebbff67ce5d9f0141121a1f
Stefan Lange <stefanlange@maeve.de>
2024-01-11 16:52:38 +0100

    fix2

--- src/util/Utility.js
+++ src/util/Utility.js
@@ -1,5 +1,8 @@
+
+// Gets a searchParameter and a the array from entries to search in, Pushing an Array of results. containing the id's of the search results
 export function searchQuery(entries, searchParameter) {
-    return entries
+    const searchResults = entries
         .filter(item => Object.values(item.fields).includes(searchParameter))
         .map(item => item.fields.id);
+    return searchResults;
 }
