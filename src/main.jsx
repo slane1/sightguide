1e1e97901a1205c385dd8d1a7cbb1c2c2f591f63
Stefan Lange <stefanlange@maeve.de>
2024-01-16 15:59:40 +0100

    datacontext file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import DataContextProvider from './contexts/DataContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DataContextProvider>
+      <App />
+    </DataContextProvider>
   </React.StrictMode>,
 )
