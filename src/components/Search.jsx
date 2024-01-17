1d1da9210936eb3a9297469a91d47c350473cac4
jb <workingj@pm.me>
2024-01-17 13:41:19 +0100

    fix fetching to async, import sortQuery

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../contexts/DataContext'
+import {sortQuery} from '../util/Sort'
 
 export default function Search(){
 const { entries, setSearchEntries } = useContext(DataContext);
@@ -31,7 +32,7 @@ function searchQuery(parameter) {
                             height="35"
                             viewBox="0 0 24 24"
                             fill="currentColor" 
-                            class="ml-3"> 
+                            className="ml-3"> 
                             <path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"></path>
                         </svg>
                 </button>
