86fe54eccc65bdaf9f500c6d54162f66923afd72
Stefan Lange <stefanlange@maeve.de>
2024-01-17 16:06:28 +0100

    seo1

--- src/components/ListView.jsx
+++ src/components/ListView.jsx
@@ -3,31 +3,41 @@ import { DataContext } from '../contexts/DataContext';
 import ListItem from './ListItem';
 
 export default function ListView() {
-  const { entries, searchEntries, displayEntries, setDisplayEntries } = useContext(DataContext);
+  const { entries } = useContext(DataContext);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [displayEntries, setDisplayEntries] = useState([]);
 
-  // useEffect(() => {
-  //   if (searchEntries.length > 0) {
-  //     setDisplayEntries(searchEntries);
-  //   } else {
-  //     setDisplayEntries(entries);
-  //   }
-  // }, [entries, searchEntries]);
-  
   useEffect(() => {
-    console.log("Entries: ", entries);
-    console.log("Search Entries: ", searchEntries);
-    if (searchEntries.length > 0) {
-      setDisplayEntries(searchEntries);
+    if (searchTerm) {
+      const filteredEntries = entries.filter(
+        (item) =>
+          item.fields.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.fields.bauzeit
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase()) ||
+          item.fields.bauherr.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setDisplayEntries(filteredEntries);
     } else {
       setDisplayEntries(entries);
     }
-  }, [entries, searchEntries]);
+  }, [entries, searchTerm]);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div className="grid  md:grid-cols-1 lg:grid-cols-6 gap-8 ">
-      {entries.map((item) => {
-        return <ListItem item={item} key={item.fields.id} />;
-      })}
+      <input
+        type='text'
+        placeholder='Search'
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      {displayEntries.map((item) => (
+        <ListItem item={item} key={item.fields.id} />
+      ))}
     </div>
   );
 }
