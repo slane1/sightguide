3b435e6640931d54b4f9b856ea94da2f5032de66
jb <workingj@pm.me>
2024-01-19 13:44:51 +0100

    we did it!

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { DataContext } from './contexts/DataContext';
-import ListView from "./components/ListView.jsx"
+import React, { useContext, useEffect, useState } from "react";
+import { DataContext } from "./contexts/DataContext";
+import ListView from "./components/ListView.jsx";
 import Footer from "./components/Footer.jsx";
+import { Route, Routes } from "react-router-dom";
+import Detail from "./components/Detail.jsx";
 
 export default function App() {
   const { loading } = useContext(DataContext);
 
   return (
     <>
-        <h1 className=" p-11   font-Poppins  sm:text-3x1 md:text-6xl ">Sightguide</h1>
-        {loading ? (
+      <h1 className=" p-11   font-Poppins  sm:text-3x1 md:text-6xl ">
+        Sightguide
+      </h1>
+      {loading ? (
         <p>Loading...</p>
-        ) : ( <ListView />)}
+      ) : (
+        <Routes>
+          <Route path="/" element={<ListView />} />
+          <Route path=":itemPath" element={<Detail />} />
+        </Routes>
+      )}
       <Footer />
     </>
   );
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,38 @@
 import Map from "./Map";
+import { useParams, useLocation } from "react-router-dom";
+import { DataContext } from "../contexts/DataContext";
+import { useContext } from "react";
+import React from "react";
 
-export default function Detail({ item }) {
+export default function Detail() {
+  const location = useLocation();
+  console.log("Detail::location: ", location);
+  const { item } = location.state;
+  // const { itemPath } = useParams();
+  // const { entries } = useContext(DataContext);
+
+  // let item2 = entries.map((dings) => {
+  //   // console.log(
+  //   //   "hallo",
+  //   //   dings.fields.name.toLowerCase().replaceAll(" ", "-") == itemPath
+  //   // );
+  //   return dings.fields.name.toLowerCase().replaceAll(" ", "-") == itemPath
+  //     ? dings
+  //     : null;
+  // });
+  // const item = item[0];
+  // console.log("Detail::item: ", item);
+
   return (
     <div className="detail" key={item.fields.id}>
       <div className="c">
+        <div className="c visual">
+          {item.fields.images.map((img) => {
+            return (
+              <img src={img.fields.file.url} alt={img.fields.file.fileName} />
+            );
+          })}
+        </div>
         <h3 className="text-3xl font-bold underline">{item.fields.name}</h3>
         <li>{item.fields.epoche}</li>
         <li>
@@ -15,15 +44,8 @@ export default function Detail({ item }) {
         <li>{item.fields.bauherr}</li>
         <li>{item.fields.beschreibung}</li>
       </div>
-      <div className="c visual">
-        {item.fields.images.map((img) => {
-          return (
-            <img src={img.fields.file.url} alt={img.fields.file.fileName} />
-          );
-        })}
-      <Map geolocation={geolocation} />
-      </div>
-
+      <br />
+      <Map geolocation={item.fields.geolocation} />
     </div>
   );
 }
