e430f8428a1318039bd9ded10dc09d667fb73a87
Oskar <oskar.radetzki.dev@googlemail.com>
2024-01-19 09:11:33 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,13 @@
 export default function Footer() {
     
-    return ("");
+    return (
+        <>
+            <div>
+                <span className="bg-current lg:pt-24 sm:m-1">&copy; 2024 SightGuide.</span>
+            </div>
+            <div>
+                <span>Powered by Group One!</span>
+            </div>
+        </>
+    );
 }
