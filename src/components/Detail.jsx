baa77c65bf457177172baf13b865f5668424482c
Stefan Lange <stefanlange@maeve.de>
2024-01-19 14:59:47 +0100

    ordering

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -11,27 +11,33 @@ export default function Detail() {
 
   return (
     <div className="flex flex-col max-w-[80%]" key={item.fields.id}>
-      <div className="flex flex-col">
-        <div className="flex flex-row mb-2 space-x-4 justify-between max-w-[10em]">
-          {item.fields.images.map((img) => {
-            return (
-              <img src={img.fields.file.url} alt={img.fields.file.fileName} />
-            );
-          })}
+      <div className="flex flex-col items-center">
+          <h3 className="text-3xl font-bold underline mb-3">{item.fields.name}</h3>
+        <div className="flex flex-row mb-2 space-x-4 justify-center max-w-[20em]">
+          {item.fields.images.map((img, index) => (
+            <img
+              key={index}
+              src={img.fields.file.url}
+              alt={img.fields.file.fileName}
+            />
+          ))}
         </div>
-        <h3 className="text-3xl font-bold underline">{item.fields.name}</h3>
-        <li>{item.fields.epoche}</li>
-        <li>
-          {item.fields.geolocation.lon + " " + item.fields.geolocation.lat}
-        </li>
-        <li>{item.fields.stadt}</li>
-        <li>{item.fields.land}</li>
-        <li>{item.fields.bauzeit}</li>
-        <li>{item.fields.bauherr}</li>
-        <li className="mb-3">{item.fields.beschreibung}</li>
+        <ul className="mb-3">
+          <li><span>Ort: </span>{item.fields.stadt}</li>
+          <li><span>Land: </span>{item.fields.land}</li>
+          <li><span>Bauzeit: </span>{item.fields.bauzeit}</li>
+          <li><span>Epoche: </span>{item.fields.epoche}</li>
+          <li><span>Bauherr </span> {item.fields.bauherr}</li>
+          <li className="mb-4">
+          <span>Geolocation: </span> {item.fields.geolocation.lon + " " + item.fields.geolocation.lat}
+          </li>
+          <li className="mb-1">{item.fields.beschreibung}</li>
+        </ul>
       </div>
       <br />
-      <Map geolocation={item.fields.geolocation} />
+      <div>
+        <Map geolocation={item.fields.geolocation} />
+      </div>
     </div>
   );
 }
